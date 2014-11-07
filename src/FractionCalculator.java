599a71349ab186e209642141047597772f7b7a51
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-11-07 07:15:44 +0000

    FractIionCalculator Basics
    Created fields and constructors only

---
+++ src/FractionCalculator.java
@@ -0,0 +1,36 @@
+//Needs string tokenizer
+
+
+public class FractionCalculator {
+
+	//Needs constructor method to be able to read in a single line 
+	
+	
+	private Fraction total;
+	private Fraction operand;
+	private String operator;
+	
+	public Fraction getTotal() {
+		return total;
+	}
+	public void setTotal(Fraction total) {
+		this.total = total;
+	}
+	public Fraction getOperand() {
+		return operand;
+	}
+	public void setOperand(Fraction operand) {
+		this.operand = operand;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	
+	
+	
+	
+	
+}
