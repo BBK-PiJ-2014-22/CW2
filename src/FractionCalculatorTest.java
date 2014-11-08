ac69a0009d0514d3f425807501261745a38a9ec9
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2014-11-08 17:52:47 +0000

    Absolute & Negate Tested
    Completed

--- src/FractionCalculatorTest.java
+++ src/FractionCalculatorTest.java
@@ -4,9 +4,7 @@ public class FractionCalculatorTest {
 	public static void main(String[] args) {
 		
 		FractionCalculator fc = new FractionCalculator();
-		
-		System.out.println(fc);
-		
+				
 		//test calculate
 		//Test add
 		testCalc(fc, new Fraction(0,1), "+", new Fraction(1,2), new Fraction(1,2), "Addition test 1 failed");
@@ -26,17 +24,23 @@ public class FractionCalculatorTest {
 		testCalc(fc, new Fraction(1,1), "/", new Fraction(1,2), new Fraction(2,1), "Divide test 3 failed");
 		testCalc(fc, new Fraction(1,2), "/", new Fraction(2,1), new Fraction(1,4), "Divide test 4 failed");
 		
-		//test multiply
-		testCalc(fc, new Fraction(0,1), "*", new Fraction(1,2), new Fraction(0,1), "Multiply test 1 failed");
-		testCalc(fc, new Fraction(1,2), "*", new Fraction(1,4), new Fraction(1,8), "Multiply test 2 failed");
-		testCalc(fc, new Fraction(1,2), "*", new Fraction(0,1), new Fraction(0,1), "Multiply test 3 failed");
-		testCalc(fc, new Fraction(1,2), "*", new Fraction(1,2), new Fraction(1,4), "Multiply test 4 failed");
+		//test Negate
+		testCalc(fc, new Fraction(0,1), "*", new Fraction(1,2), new Fraction(0,1), "Negate test 1 failed");
+		testCalc(fc, new Fraction(1,2), "*", new Fraction(1,4), new Fraction(1,8), "Negate test 2 failed");
+		testCalc(fc, new Fraction(1,2), "*", new Fraction(0,1), new Fraction(0,1), "Negate test 3 failed");
+		testCalc(fc, new Fraction(1,2), "*", new Fraction(1,2), new Fraction(1,4), "Negate test 4 failed");
+		
+		//test Absolute - f2 is redundant
+		testCalc(fc, new Fraction(0,1), "a", new Fraction(0,1), new Fraction(0,1), "Absolute test 1 failed");
+		testCalc(fc, new Fraction(1,-2), "a", new Fraction(0,1), new Fraction(1,2), "Absolute test 2 failed");
+		testCalc(fc, new Fraction(1,2), "a", new Fraction(0,1), new Fraction(1,2), "Absolute test 3 failed");
 		
 		
+		//test Negate - f2 is redundant
+		testCalc(fc, new Fraction(1,-2), "n", new Fraction(0,1), new Fraction(1,2), "Negate test 1 failed");
+		testCalc(fc, new Fraction(1,2), "n", new Fraction(0,1), new Fraction(1,-2), "Negate test 2 failed");
+		testCalc(fc, new Fraction(0,1), "n", new Fraction(0,1), new Fraction(0,1), "Negate test 3 failed");		
 		
-		
-		//test negate
-
 		System.out.println(fc);
 		System.out.println("Tests complete");
 	}
@@ -46,9 +50,16 @@ public class FractionCalculatorTest {
 	
 	
     static void testCalc(FractionCalculator calc, Fraction f1, String symbol, Fraction f2, Fraction result, String msg){
-    	
+    	    	
     	calc.setTotal(f1);
-    	calc.calculate(symbol, f2);
+    	
+    	if (symbol.equals("a")){
+    		calc.absolute();
+    	} else if (symbol.equals("n")){
+    		calc.negate();
+    	} else{
+    		calc.calculate(symbol, f2);
+    	}
     
     	if (! calc.getTotal().equals(result)){
     		System.out.println(msg);
